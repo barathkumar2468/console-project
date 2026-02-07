f2cfd839bd8139702161f34c41dfcbb887667871
barathkumar2468 <137084001+barathkumar2468@users.noreply.github.com>
2026-02-07 09:28:50 +0530

    Create LoanApplicant.java

---
+++ LoanApplicant.java
@@ -0,0 +1,10 @@
+public class LoanApplicant {double income;
+    int age;
+    int creditScore;
+
+    public LoanApplicant(double income, int age, int creditScore) {
+        this.income = income;
+        this.age = age;
+        this.creditScore = creditScore;
+    }
+}
